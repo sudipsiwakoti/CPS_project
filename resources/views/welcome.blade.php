55cee8c4d6e5b3eaf29441eee00d64549f12ce27
Christian Cabato <Blinkzs@DESKTOP-Q353JSJ.localdomain>
2019-09-30 12:01:19 +1000

    [CC] Default login and register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Course Planning System
                 </div>
 
                 <div class="links">
