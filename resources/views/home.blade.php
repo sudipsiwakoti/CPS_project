1d373031593ea69b67514f3a9231e7b90d36c84a
Tristan Ofner <tristanofner@gmail.com>
2019-10-18 23:30:12 +0000

    [TO] User-role dependent home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,12 +17,15 @@
 
                     <p> This web application provides users with the capability to develop a timetable for their subjects for
                     preparation and the reduction of stress in their university career. </p>
-                    <p> The <a href="{{ url('/planning') }}">Planning </a> page will showcase the app's main feature
-                        of being able to structure a course plan based on your preferences. </p>
                     <p> The <a href="{{ url('/subjects') }}">Subjects</a> page will allow you to search for subjects. </p>
                     <p> The <a href="{{ url('/courses') }}">Courses</a> page will allow you to search for courses. </p>
+                    @if (Auth::user()->role == 0)
+                    <p> The <a href="{{ url('/planning') }}">Planning </a> page will showcase the app's main feature
+                        of being able to structure a course plan based on your preferences. </p>
                     <p> The <a href="{{ url('/results') }}">Results</a> page will allow you to add or modify your results to get an overview of your performance. </p>
-
+                    @else
+                    <p> The <a href="{{ url('/enrolments') }}">Enrolments</a> page will allow you to view the forecasted enrolments in the subjects you administer or will administer. </p>
+                    @endif
                 </div>
             </div>
         </div>
